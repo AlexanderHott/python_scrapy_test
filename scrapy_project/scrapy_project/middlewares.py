7c6ad1c6e7c3ef647cc20e5fdfb6ee0c9f49a889
AlexanderHOtt <aott01@protonmail.ch>
2022-09-06 02:22:06 -0700

    finish

--- scrapy_project/scrapy_project/middlewares.py
+++ scrapy_project/scrapy_project/middlewares.py
@@ -53,7 +53,7 @@ class ScrapyProjectSpiderMiddleware:
             yield r
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
 
 
 class ScrapyProjectDownloaderMiddleware:
@@ -100,4 +100,4 @@ class ScrapyProjectDownloaderMiddleware:
         pass
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
